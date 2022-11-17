9060bfff39ec6e8dcd8436d3bb46c37d1b866f6a
Maksym Bernatskyi <bernasik007@gmail.com>
2022-11-17 23:54:48 +0200

    profile done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
+import Profile from './components/Profile/Profile';
+import user from './data/user.json';
+// import Statistics from './components/Statistics/Statistics';
+// import data from './data/data.json';
+// import FriendList from './components/FriendList/FriendList';
+// import friends from './data/friends.json';
+// import TransactionsHistory from './components/TransactionHistory/TransactionHistory';
+// import transactions from './data/transactions.json';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats} 
+      />
+      {/* <Statistics title="UPLOAD STATS" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionsHistory items={transactions} /> */}
+      </>
   );
 };
