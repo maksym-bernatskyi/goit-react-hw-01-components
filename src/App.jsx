a39a60f6163c8583a570538c27a8fb03f45dc93c
Maksym Bernatskyi <bernasik007@gmail.com>
2022-11-21 21:49:57 +0200

    task-2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Profile from './components/Profile/Profile';
 import user from './data/user.json';
-// import Statistics from './components/Statistics/Statistics';
-// import data from './data/data.json';
+import Statistics from './components/Statistics/Statistics';
+import data from './data/data.json';
 // import FriendList from './components/FriendList/FriendList';
 // import friends from './data/friends.json';
 // import TransactionsHistory from './components/TransactionHistory/TransactionHistory';
@@ -17,9 +17,9 @@ export default function App() {
       avatar={user.avatar}
       stats={user.stats} 
       />
-      {/* <Statistics title="UPLOAD STATS" stats={data} />
+      <Statistics title="UPLOAD STATS" stats={data} />
       <Statistics stats={data} />
-      <FriendList friends={friends} />
+      {/* <FriendList friends={friends} />
       <TransactionsHistory items={transactions} /> */}
       </>
   );
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,30 @@
+import styles from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+export default function Statistics({ title, stats }) {
+    const renderTitle = title ? <h2 className={styles.title}>UPLOAD STATS</h2> : '';
+    return (
+        <section className={styles.statistics}>
+            {renderTitle}
+            <ul className={styles.stat__list}>
+                {stats.map(({ id, label, percentage }) => (
+                <li className={styles.item} key={id}>
+                <span className={styles.label}>{label}</span>
+                <span className={styles.percentage}>{percentage}</span>
+                </li>
+                ))}
+            </ul>
+        </section>
+    )
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string.isRequired,
+    stats: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired
+        })
+    )
+}
